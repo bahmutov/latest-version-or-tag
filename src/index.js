2caac1fba81193005493963bd9369ecadfb5c497
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-07 22:22:44 -0500

    feat(code): add code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const decide = require('./decide-start-version')
+
+function printVersion (version) {
+  console.log('latest version', version)
+}
+
+function onError (err) {
+  console.error('Could not determine latest version')
+  console.error(err)
+}
+
+decide()
+  .then(printVersion)
+  .catch(onError)
