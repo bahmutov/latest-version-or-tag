ab71bc930821133fef009b5c494e09ff363c5fde
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-07 22:38:39 -0500

    feat(tags): fetch git tags before deciding version, close #1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 'use strict'
 
+const ggit = require('ggit')
 const decide = require('./decide-start-version')
 
-module.exports = decide
+function latest () {
+  return ggit.fetchTags()
+    .then(decide)
+}
+
+module.exports = latest
